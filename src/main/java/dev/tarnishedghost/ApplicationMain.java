9748ea3aaabb7d98f711477164ed0a02bfdf35a7
TarnishedGhost <biliy.bohdan@lll.kpi.ua>
2024-12-28 10:45:27 +0000

    Initial commit

---
+++ src/main/java/dev/tarnishedghost/ApplicationMain.java
@@ -0,0 +1,13 @@
+package dev.tarnishedghost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+
+}

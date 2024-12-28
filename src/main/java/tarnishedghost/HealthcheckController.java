78305d4f4e6f799a78e18898eabf7d3ce3df1047
TarnishedGhost <biliy.bohdan@lll.kpi.ua>
2024-12-28 13:53:23 +0000

    Added HealthcheckController

---
+++ src/main/java/tarnishedghost/HealthcheckController.java
@@ -0,0 +1,28 @@
+package tarnishedghost;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthcheckController {
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<Map<String, Object>> healthcheck() {
+        Map<String, Object> response = new HashMap<>();
+        ZoneId kyivZone = ZoneId.of("Europe/Kiev");
+        ZonedDateTime now = ZonedDateTime.now(kyivZone);
+
+        String formattedDate = now.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+
+        response.put("Status", "Service is working as intended.");
+        response.put("Data", formattedDate);
+        return ResponseEntity.ok(response);
+    }
+}
